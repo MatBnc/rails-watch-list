df4b07793bb268db6f08fc2acb051c0e5ba4a12b
matteobianchi <matteo.bianchi@hec.edu>
2022-08-18 15:35:40 +0200

    Mon appli marche - je vais maintenant la casser

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :lists do
+    resource :bookmarks, only: [:new, :create]
+  end
 
+  delete 'bookmarks/:id', to: 'bookmarks#destroy', as: :del_bookmark
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
