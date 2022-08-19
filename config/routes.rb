b8fdc73fee9ab9ec2935b21e440175ac2a05d2a7
matteobianchi <matteo.bianchi@hec.edu>
2022-08-19 11:05:48 +0200

    Watch list kinda updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :lists do
+  resources :lists, except: [:destroy] do
     resource :bookmarks, only: [:new, :create]
   end
 
+  delete 'lists/:id', to: 'lists#destroy', as: :del_list
   delete 'bookmarks/:id', to: 'bookmarks#destroy', as: :del_bookmark
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
